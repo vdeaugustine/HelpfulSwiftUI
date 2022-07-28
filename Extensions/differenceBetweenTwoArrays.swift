30b378dccc17a936d105e274480eb16ab92218db
vdeaugustine <vinnied44@csu.fullerton.edu>
2022-07-28 11:46:07 -0700

    added first Extensions

---
+++ Extensions/differenceBetweenTwoArrays.swift
@@ -0,0 +1,7 @@
+extension Array where Element: Hashable {
+    func difference(from other: [Element]) -> [Element] {
+        let thisSet = Set(self)
+        let otherSet = Set(other)
+        return Array(thisSet.symmetricDifference(otherSet))
+    }
+}
---
+++ Extensions/hideKeyboard.swift
@@ -0,0 +1,27 @@
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
